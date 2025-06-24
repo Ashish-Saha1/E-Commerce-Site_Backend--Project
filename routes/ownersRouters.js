1b9d5235aa4078a60d3732c5036637619b8daf73
Ashish Saha <rubelsaha1@gamil.com>
2025-06-24 19:20:29 +0600

    save-4

--- routes/ownersRouters.js
+++ routes/ownersRouters.js
@@ -1,9 +1,10 @@
 const express = require('express'); 
 const router = express.Router();
+const OwenerModel = require('../models/ownerModel');
+const OwnerModel = require('../models/ownerModel');
 
-
-console.log("Owner Route:",process.env.NODE_ENV);
-console.log("Owner Route2:",process.env.check);
+// console.log("Owner Route:",process.env.NODE_ENV);
+// console.log("Owner Route2:",process.env.check);
 
 router.get('/', (req,res)=>{
 
@@ -12,8 +13,21 @@ router.get('/', (req,res)=>{
 
 if(process.env.NODE_ENV === 'development'){
 
-    router.get('/create', (req,res)=>{
-    res.send(`Owner Create Successfull`)
+    router.post('/create', async (req,res)=>{
+        const {fullname, email, password} = req.body;
+
+        const isOwner = await OwnerModel.find();
+
+        if(isOwner.length>0){
+            return res.status(500).send('Only one owner is accepted')
+        }
+        const owner = await OwnerModel.create({
+            fullname,
+            email,
+            password
+        })
+
+        res.status(201).send(owner)
     })
     
 }
