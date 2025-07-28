8566a79bb612819afeef30a84f0c5a001ae82b24
ashish-saha <rubelsaha1@gmail.com>
2025-07-28 08:14:56 +0600

    working on cart delete

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -12,6 +12,7 @@ const { registerController,
 
         } = require('../controller/userController');
 const upload = require('../config/multerUpload');
+const flash = require('flash');
 
 
 
@@ -68,8 +69,22 @@ router.get('/profile', (req,res,next)=>{
 
 
 //Cart
-router.get('/cart', (req,res,next)=>{
-    res.render('cart') 
+router.get('/cart', isLoggedIn, async (req,res,next)=>{
+    const locals = {
+        title : 'Cart'
+    }
+
+    const userId = req.user._id;
+    const currentUser = await UserModel.findById(userId);
+
+    const totalAmount = currentUser.cart.reduce((total, num)=>{
+        return Number(total) + Number(num.price)
+    }, 0)
+
+    console.log(currentUser);
+    
+
+    res.render('cart', {locals, totalAmount}) 
 })
 
 
@@ -80,8 +95,16 @@ router.post('/cart/:productId',isLoggedIn, async (req,res,next)=>{
             const userId = req.user._id
 
             const user = await UserModel.findOne(userId)
-             const product = await ProductModel.findById(productId);
-        // console.log(productId, userId)
+            const product = await ProductModel.findById(productId);
+                //The first matching object from the user.cart array where the condition is true
+                //Or undefined if no match is found.
+            const hasCartItem = user.cart.find((item)=> item._id.toString() === productId)
+            
+            if(hasCartItem){
+                req.flash('successMsg', "Product has already Added to Cart")
+                return res.redirect('/users/cart')
+            }
+            
             user.cart.push(product);
             //user.cart.push({ product: productId, quantity: 1 });
 
@@ -96,6 +119,34 @@ router.post('/cart/:productId',isLoggedIn, async (req,res,next)=>{
 
 
 })
+
+
+const mongoose = require('mongoose');
+
+router.post('/remove-from-cart/:productId', async (req, res) => {
+  try {
+    const productId = req.params.productId;
+    const userId = req.user._id;
+
+    if (!mongoose.Types.ObjectId.isValid(productId)) {
+      return res.status(400).send("Invalid product ID");
+    }
+
+    const deleted = await UserModel.findByIdAndUpdate(userId, {
+      $pull: {
+        cart: {
+          _id: new mongoose.Types.ObjectId(productId)
+        }
+      }
+    });
+
+    res.redirect('/users/cart');
+  } catch (error) {
+    console.log(error.message);
+    res.send('Something went wrong in cart delete route');
+  }
+});
+
 
 
 
